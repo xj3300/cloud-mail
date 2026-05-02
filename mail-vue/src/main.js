faa45f7f0ca73668e7d84f7dba851580e99ea336
ㅤ <63586817+xj3300@users.noreply.github.com>
2026-05-02 08:02:56 +0800

    Update main.js

--- mail-vue/src/main.js
+++ mail-vue/src/main.js
@@ -8,11 +8,29 @@ import piniaPersistedState from 'pinia-plugin-persistedstate';
 import 'element-plus/theme-chalk/dark/css-vars.css';
 import 'nprogress/nprogress.css';
 import perm from "@/perm/perm.js";
-const pinia = createPinia().use(piniaPersistedState)
 import i18n from "@/i18n/index.js";
+import { useUiStore } from '@/store/ui.js';
+
+const pinia = createPinia().use(piniaPersistedState)
+
 const app = createApp(App).use(pinia)
+
 await init()
+
 app.use(router).use(i18n).directive('perm',perm)
+
+const uiStore = useUiStore()
+
+if (uiStore.dark) {
+  document.documentElement.classList.add('dark')
+
+  const metaTag = document.getElementById('theme-color-meta')
+  if (metaTag) {
+    const isDesktop = window.matchMedia('(pointer: fine) and (hover: hover)').matches
+    metaTag.setAttribute('content', isDesktop ? '#141414' : '#000000')
+  }
+}
+
 app.config.devtools = true;
 
 app.mount('#app');
